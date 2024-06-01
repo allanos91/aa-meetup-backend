071abc32737a0a9d98871b1b53d8b0af0dd5511c
Alexander <allanosinhb@gmail.com>
2024-06-01 14:26:20 -0700

    login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,25 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
+import { restoreCSRF, csrfFetch} from './store/csrf'
+import * as sessionActions from './store/session';
 
 const store = configureStore();
+
+if (import.meta.env.MODE !== "production") {
+  restoreCSRF();
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions; // <-- ADD THIS LINE
+}
+
+
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
