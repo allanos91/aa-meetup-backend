2804b471832060da628948c117336611a1a18850
Alexander <allanosinhb@gmail.com>
2024-06-02 20:25:34 -0700

    phase 2 authenticate me

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,48 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    })
+  }, [dispatch])
+
+  return (
+    <>
+    {isLoaded && <Outlet />}
+    </>
+  )
+}
+
+
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <h1>Welcome!</h1>
-  },
-  {
-    path: '/login',
-    element: <LoginFormPage/>
+    element: <Layout/>,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: '/login',
+        element: <LoginFormPage/>
+      },
+      {
+        path: "/signup",
+        element: <SignupFormPage/>
+      }
+    ]
   }
 ])
-
-
-
 
 function App() {
   return <RouterProvider router={router} />;
