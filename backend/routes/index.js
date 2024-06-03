0b41c0538687917417f88446b512587ec639adb4
Alexander <allanosinhb@gmail.com>
2024-06-03 01:51:00 -0700

    update backend routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -29,21 +29,24 @@ if (process.env.NODE_ENV === 'production') {
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/build")));
+  router.use(express.static(path.resolve("../frontend/dist")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 }
 
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
+    const csrfToken = req.csrfToken()
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    })
   });
 }
 
