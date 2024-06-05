5503d24e88252c76e1663eadc061342d09e9ac71
Alexander <allanosinhb@gmail.com>
2024-06-04 20:54:15 -0700

    fixed console.logs

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -10,17 +10,17 @@ const LandingPage = () => {
 
     return (
         <span>
-        <div className='section-one'>
+        <section className='section-one'>
             <p id='section-one-desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste aperiam eveniet dolore nobis molestias cupiditate, culpa voluptate a, magni aliquid quidem. Beatae autem nesciunt fuga facilis neque temporibus sint expedita.</p>
             <p id='section-one-info'> THIS IS WHERE INFOGRAPH GOES</p>
-        </div>
-        <div className='section-two'>
+        </section>
+        <section className='section-two'>
             <h2 className='section-two-desc' id="section-two-title">How Cleet Up! works</h2>
             <h3 className='section-two-desc'>1. Find a sports group in your area</h3>
             <h3 className='section-two-desc'>2. Join an event: free or paid</h3>
             <h3 className='section-two-desc'>3. CLEET UP!</h3>
-        </div>
-        <div className='section-three'>
+        </section>
+        <section className='section-three'>
             <div className='column-one'>
                 <p>Icon here</p>
                 <a>See all groups</a>
@@ -36,7 +36,10 @@ const LandingPage = () => {
                 <a>Start a new group</a>
                 <p>caption</p>
             </div>
-        </div>
+        </section>
+        <section className='section-four'>
+            <button>Join Cleet Up!</button>
+        </section>
         </span>
     )
 }
