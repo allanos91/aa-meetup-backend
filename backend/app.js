6505caf75fda291f291f718add56c23ec1d7b14a
Alexander <allanosinhb@gmail.com>
2024-06-09 02:04:29 -0700

    minor change to include description in event details

--- backend/app.js
+++ backend/app.js
@@ -74,7 +74,6 @@ app.use((err, _req, _res, next) => {
 // Error formatter
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
-  console.error(err);
   res.json({
     message: err.message,
     errors: err.errors
