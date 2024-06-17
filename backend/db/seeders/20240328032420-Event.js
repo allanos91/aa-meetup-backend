dd8a12c2ae36578d79653d7f8065c02fa91a676e
Alexander <allanosinhb@gmail.com>
2024-06-16 23:15:49 -0700

    added some seeder files

--- backend/db/seeders/20240328032420-Event.js
+++ backend/db/seeders/20240328032420-Event.js
@@ -21,47 +21,47 @@ module.exports = {
     {
       groupId: 1,
       venueId: 1,
-      name: "Alex's groovy hangout.",
-      type: "Online",
-      startDate: new Date('April 20, 2024 5:00:00'),
-      endDate: new Date('April 20, 2024 17:00:00'),
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius exercitationem eaque ea, ducimus molestiae similique tempore cum saepe ut perspiciatis, sed amet eos quidem quia molestias voluptates necessitatibus esse ipsa!'
+      name: "Wide Reciever Clinic #1 - Fundamentals",
+      type: "In Person",
+      startDate: new Date('August 20, 2024 5:00:00'),
+      endDate: new Date('August 20, 2024 8:00:00'),
+      description: 'Learning and practicing the fundamentals of route running.'
     },
     {
       groupId: 1,
       venueId: 2,
-      name: "Jordan Love's groovy hangout.",
+      name: "3 on 3 Scrims",
       type: "Online",
-      startDate: new Date('April 20, 2024 5:00:00'),
-      endDate: new Date('April 20, 2024 17:00:00'),
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius exercitationem eaque ea, ducimus molestiae similique tempore cum saepe ut perspiciatis, sed amet eos quidem quia molestias voluptates necessitatibus esse ipsa!'
+      startDate: new Date('August 23, 2024 5:00:00'),
+      endDate: new Date('August 23, 2024 8:00:00'),
+      description: 'Round robin style scrimmages. 3 recievers vs 3 defenders'
     },
     {
       groupId: 2,
       venueId: 3,
-      name: "Amon Ra's groovy hangout.",
+      name: "Patty's chill kickback.",
       type: "Online",
-      startDate: new Date('April 20, 2024 5:00:00'),
-      endDate: new Date('April 20, 2024 17:00:00'),
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius exercitationem eaque ea, ducimus molestiae similique tempore cum saepe ut perspiciatis, sed amet eos quidem quia molestias voluptates necessitatibus esse ipsa!'
+      startDate: new Date('September 20, 2024 12:00:00'),
+      endDate: new Date('Septmember 20, 2024 18:00:00'),
+      description: 'A chill hangout where me and Trav explain how we will do it again.'
     },
     {
       groupId: 2,
       venueId: 4,
       name: "Puka's groovy hangout.",
       type: "Online",
-      startDate: new Date('April 20, 2024 16:00:00'),
-      endDate: new Date('April 20, 2024 17:00:00'),
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius exercitationem eaque ea, ducimus molestiae similique tempore cum saepe ut perspiciatis, sed amet eos quidem quia molestias voluptates necessitatibus esse ipsa!'
+      startDate: new Date('September 21, 2024 16:00:00'),
+      endDate: new Date('September 21, 2024 17:00:00'),
+      description: "A review over yesterday's kickback."
     },
     {
       groupId: 2,
       venueId: 5,
-      name: "Jessica's groovy hangout.",
+      name: "Playing catch with the boys",
       type: "Online",
-      startDate: new Date('April 20, 2024 16:00:00'),
-      endDate: new Date('April 20, 2024 17:00:00'),
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius exercitationem eaque ea, ducimus molestiae similique tempore cum saepe ut perspiciatis, sed amet eos quidem quia molestias voluptates necessitatibus esse ipsa!'
+      startDate: new Date('October 22, 2024 16:00:00'),
+      endDate: new Date('October 22, 2024 17:00:00'),
+      description: 'Today we play catch and touch grass for a solid hour. Come join the fun!'
     },
     {
       groupId: 3,
@@ -122,8 +122,8 @@ module.exports = {
       venueId: 12,
       name: "Nathan's groovy hangout.",
       type: "Online",
-      startDate: new Date('April 20, 2024 16:00:00'),
-      endDate: new Date('April 20, 2024 17:00:00'),
+      startDate: new Date('January 20, 2025 16:00:00'),
+      endDate: new Date('April 20, 2025 17:00:00'),
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius exercitationem eaque ea, ducimus molestiae similique tempore cum saepe ut perspiciatis, sed amet eos quidem quia molestias voluptates necessitatibus esse ipsa!'
     }
    ])
